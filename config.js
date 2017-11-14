fd078fe9f21a6d894b6625dbdb13e151e757e581
Will Tomlinson <watomlinson@gmail.com>
2017-11-14 12:48:15 -0500

    formalizing config.js

--- config.js
+++ config.js
@@ -1,6 +1,22 @@
+var fs = require('fs')
+var confJson = {}
+// Check for config files
+if (fs.existsSync("./config.json")) {
+    confJson = JSON.parse(fs.readFileSync('./config.json','utf8'))
+} else if (fs.existsSync("/etc/ecr-proxy-node/config.json")) {
+    confJson = JSON.parse(fs.readFileSync('/etc/ecr-proxy-node/config.json','utf8'))
+}
+
 var config = {}
-config.remoteHost = '559139583778.dkr.ecr.us-east-1.amazonaws.com'
-config.remotePort = '443'
-config.localHost = 'docker.ar53.wfu.edu'
-config.localPort = '5000'
+config.remoteHost = confJson.remoteHost || process.env.ECR_PROXY_REMOTE_HOST
+config.remotePort = confJson.remotePort || process.env.ECR_PROXY_REMOTE_PORT || '443'
+config.localHost = confJson.localHost || process.env.ECR_PROXY_LOCAL_HOST || 'localhost' 
+config.localPort = confJson.localPort || process.env.ECR_PROXY_LOCAL_PORT || '5000'
+
+// Fail if any required values are missing
+if (!config.remoteHost){
+    console.log("remoteHost is required")
+    process.exit(1)
+}
+
 module.exports = config
--- server.js
+++ server.js
@@ -1,16 +1,20 @@
-var https = require('https'),
-    httpProxy = require('http-proxy'),
-    AWS = require('aws-sdk'),
-    fs = require('fs'),
-    config = require('./config.js'),
-    token;
+var https = require('https');
+var winston = require('winston');
+var httpProxy = require('http-proxy');
+var AWS = require('aws-sdk');
+var fs = require('fs');
+var config = require('./config.js');
+var token;
 
-function dateprint() {
-    var now = new Date()
-    //var args = Array.prototype.slice.call(arguments)
-    var args = Array.from(arguments)
-    console.log(now,args.join(' '))
-}
+// Create a logger instance
+const logger = new winston.Logger({
+    transports: [
+        new winston.transports.Console({
+            level: 'debug',
+            timestamp: true
+        })
+    ]
+});
 
 // Silly string replace function
 function parse(str) {
@@ -29,25 +33,14 @@ function setToken() {
     var ecr = new AWS.ECR({"region":"us-east-1"});
     ecr.getAuthorizationToken(params, function(err,data){
         if (err) {
-            dateprint(err, err.stack)
+            logger.error(err, err.stack)
             setTimeout(setToken,3600000)
         } else {
-            dateprint("Token successfully acquired")
+            logger.info("Token successfully acquired")
             token = data
             setTimeout(setToken,14400000)
         }
     });
-}
-
-function setOutLocation(loc){
-    var re = new RegExp(config.localHost,"g")
-    loc.replace(re,config.remoteHost)
-    return loc
-}
-function setInLocation(loc){
-    var re = new RegExp(config.remoteHost,"g")
-    loc.replace(re,config.localHost)
-    return loc
 }
 
 //
@@ -58,7 +51,7 @@ setToken()
 //
 // Create a proxy server with custom application logic
 //
-dateprint("Starting proxy for", config.remoteHost)
+logger.info("Starting proxy for", config.remoteHost + ":" + config.remotePort, "on", config.localHost + ":" + config.localPort)
 var proxy = httpProxy.createProxyServer({
     ssl: {
         key: fs.readFileSync('server.key','utf-8'),
@@ -72,32 +65,12 @@ var proxy = httpProxy.createProxyServer({
 proxy.on('proxyReq', function(proxyReq, req, res, options) {
     proxyReq.setHeader('Host', config.remoteHost);
     proxyReq.setHeader('Authorization', "Basic " + token.authorizationData[0].authorizationToken);
-    dateprint(req.connection.remoteAddress,req.method,req.url);
+    logger.info(req.connection.remoteAddress,req.method,req.url);
 });
 
 proxy.on('proxyRes', function(proxyRes,req,res){
     var loc = String(proxyRes.headers.location)
     var re = new RegExp(config.remoteHost,"g")
     var fixedloc = loc.replace(re,config.localHost + ':' + config.localPort)
+    proxyRes.headers.location = fixedloc
 });
-
-//
-// Create your custom server and just call `proxy.web()` to proxy
-// a web request to the target passed in the options
-// also you can use `proxy.ws()` to proxy a websockets request
-//
-//const opts = {
-    //ssl: {
-        //key: fs.readFileSync('server.key','utf-8'),
-        //cert: fs.readFileSync('server.cert','utf-8')
-    //}
-//};
-
-//var server = https.createServer(opts, function(req, res) {
-  // You can define here your custom logic to handle the request
-  // and then proxy the request.
-    //proxy.web(req, res, { target: parse('https://%s:443', config.remoteHost) });
-//});
-
-//console.log("listening on port config.localPort")
-//server.listen(config.localPort,'127.0.0.1');
