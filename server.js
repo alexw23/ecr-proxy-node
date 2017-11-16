bef38012308ca966c72e182bccc2deaa4abf4ce8
Will Tomlinson <watomlinson@gmail.com>
2017-11-15 22:07:00 -0500

    fix indent on all .js files

--- server.js
+++ server.js
@@ -27,29 +27,29 @@ var token;
 
 // Silly string replace function
 function parse(str) {
-    var args = [].slice.call(arguments, 1),
-        i = 0;
+  var args = [].slice.call(arguments, 1),
+    i = 0;
 
     return str.replace(/%s/g, function() {
-        return args[i++];
-    });
+      return args[i++];
+  });
 }
 
 // Sets the token needed for ECR access periodically, default every 6 hours,
 // retry failed tokens every 1 hour.
 function setToken() {
-    var params = {};
-    var ecr = new AWS.ECR({"region":"us-east-1"});
-    ecr.getAuthorizationToken(params, function(err,data){
-        if (err) {
-            logger.error(err.code + " - " + err.message)
-            setTimeout(setToken,3600000)
-        } else {
-            logger.info("Token successfully acquired")
-            token = data
-            setTimeout(setToken,14400000)
-        }
-    });
+  var params = {};
+  var ecr = new AWS.ECR({"region":"us-east-1"});
+  ecr.getAuthorizationToken(params, function(err,data){
+    if (err) {
+      logger.error(err.code + " - " + err.message)
+      setTimeout(setToken,3600000)
+    } else {
+      logger.info("Token successfully acquired")
+      token = data
+      setTimeout(setToken,14400000)
+    }
+  });
 }
 
 /*
@@ -62,33 +62,33 @@ setToken()
 // Create a proxy server with custom application logic
 var options = {};
 if (config.useSsl == 'true'){
-    options = {
-        ssl: {
-            key: fs.readFileSync('server.key','utf-8'),
-            cert: fs.readFileSync('server.cert','utf-8')
-        },
-        target: 'https://' + config.remoteHost + ":" + config.remotePort,
-        secure:false,
-    }
+  options = {
+    ssl: {
+      key: fs.readFileSync('server.key','utf-8'),
+      cert: fs.readFileSync('server.cert','utf-8')
+    },
+    target: 'https://' + config.remoteHost + ":" + config.remotePort,
+    secure:false,
+  }
 } else {
-    options = {
-        target: 'https://' + config.remoteHost + ":" + config.remotePort,
-        secure:false,
-    }
+  options = {
+    target: 'https://' + config.remoteHost + ":" + config.remotePort,
+    secure:false,
+  }
 }
+
 logger.info("Starting proxy for", config.remoteHost + ":" + config.remotePort, "on", config.localHost + ":" + config.localPort)
 var proxy = httpProxy.createProxyServer(options).listen(config.localPort);
 
-
 proxy.on('proxyReq', function(proxyReq, req, res, options) {
-    proxyReq.setHeader('Host', config.remoteHost);
-    proxyReq.setHeader('Authorization', "Basic " + token.authorizationData[0].authorizationToken);
-    logger.info(req.connection.remoteAddress,req.method,req.url);
+  proxyReq.setHeader('Host', config.remoteHost);
+  proxyReq.setHeader('Authorization', "Basic " + token.authorizationData[0].authorizationToken);
+  logger.info(req.connection.remoteAddress,req.method,req.url);
 });
 
 proxy.on('proxyRes', function(proxyRes,req,res){
-    var loc = String(proxyRes.headers.location)
-    var re = new RegExp(config.remoteHost,"g")
-    var fixedloc = loc.replace(re,config.localHost + ':' + config.localPort)
-    proxyRes.headers.location = fixedloc
+  var loc = String(proxyRes.headers.location)
+  var re = new RegExp(config.remoteHost,"g")
+  var fixedloc = loc.replace(re,config.localHost + ':' + config.localPort)
+  proxyRes.headers.location = fixedloc
 });
